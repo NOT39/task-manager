d48da61fcfde625fc72011d8d0473d879d3f785f
Not <110069021+NOT39@users.noreply.github.com>
2024-05-05 10:26:39 -0300

    chore: enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import { tasks_routes } from './routes/tasks'
 import { errorHandler } from './middlewares/error-handler'
 
 export const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
