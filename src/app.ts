7464ebf06f17ea64cff5cc5e529dc18a541a1d03
Not <110069021+NOT39@users.noreply.github.com>
2024-05-10 16:12:00 -0300

    fix: allow origin

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,12 @@ export const app = express()
 
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+
+  next()
+})
+
 app.use(express.json())
 
 app.use(cookieParser())
