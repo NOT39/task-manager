16a36a9d6d89284041c0db2338bea0d577c6c6bc
Not <110069021+NOT39@users.noreply.github.com>
2024-05-10 16:21:58 -0300

    test: try to fix cors with credentials

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,15 @@ import { errorHandler } from './middlewares/error-handler'
 
 export const app = express()
 
-app.use(cors())
-
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  // res.header('Access-Control-Allow-Origin', req.headers.origin)
+
+  const corsOptions = {
+    origin: [req.headers.origin],
+    optionsSuccessStatus: 200,
+  }
+
+  return cors(corsOptions)
 
   next()
 })
