aaffd6c23ca939e503cfbb8d0c3868bf36073aa9
Not <110069021+NOT39@users.noreply.github.com>
2024-08-02 15:26:30 -0300

    Fix: change cors solution

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,18 @@ import cors from 'cors'
 
 import { tasks_routes } from './routes/tasks'
 import { errorHandler } from './middlewares/error-handler'
+import { setHeaders } from './middlewares/set-headers'
 
 export const app = express()
 
-const corsOptions = {
-  origin: ['http://localhost:5500', 'http://localhost:5500', '*'],
-  optionsSuccessStatus: 200,
-}
+// const corsOptions = {
+// origin: '*',
+// optionsSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+
+app.use(setHeaders)
 
 app.use(express.json())
 
